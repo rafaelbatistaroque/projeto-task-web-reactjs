4f59f68841c92f8bb93fa4ee17402e037308cf91
Rafael Batista <maranata_rafa@hotmail.com>
2020-10-14 19:06:15 -0400

    Foi criado footer e com estilo e ajustes. ✔️

--- src/App/Components/Footer/Footer.js
+++ src/App/Components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={`${styles.footer} container`}>Footer</div>;
+  return (
+    <small className={`${styles.footer} container`}>
+      Task - Organizar é nossa tarefa | Quase todos os direitos reservados
+    </small>
+  );
 };
 
 export default Footer;
