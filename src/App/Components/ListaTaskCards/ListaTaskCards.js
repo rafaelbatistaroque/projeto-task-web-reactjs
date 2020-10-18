1e34d847a43ea9ff8b6e15ac4dc1bed9de59f2fe
Rafael Batista <maranata_rafa@hotmail.com>
2020-10-18 01:42:27 -0400

    Criado card de task com estilo 😎 e ajustes👨‍🔧.

--- src/App/Components/ListaTaskCards/ListaTaskCards.js
+++ src/App/Components/ListaTaskCards/ListaTaskCards.js
@@ -1,8 +1,16 @@
 import React from "react";
+import TaskCard from "../TaskCard/TaskCard";
 import styles from "./ListaTaskCards.module.css";
 
 const ListaTaskCards = () => {
-  return <div className={styles.listaTaskCards}></div>;
+  return (
+    <div className={`${styles.listaTask} container`}>
+      <h1>TAREFAS</h1>
+      <div className={styles.listaTaskCards}>
+        <TaskCard />
+      </div>
+    </div>
+  );
 };
 
 export default ListaTaskCards;
