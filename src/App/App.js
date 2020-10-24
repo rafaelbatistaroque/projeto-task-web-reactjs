60d8fdd00827f5b8986a38ef8fe6f66c30b32654
Rafael Batista <maranata_rafa@hotmail.com>
2020-10-24 19:46:45 -0400

    Ajustes em nomes de pastas.

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import { Task } from "./Hooks/TaskContext";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { Task } from "./hooks/TaskContext";
 import HomePage from "./views/Home/HomePage";
 
 function App() {
